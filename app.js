2dbdaff4809c30020d02469d589fdcd0bd0aa8c3
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-05-13 14:00:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var sdcGithub = angular.module('sdcGithub', ['ngRoute']);
 
-sdcGithub.controller('IndexCtrl', function ($scope, $route) {
+sdcGithub.controller('IndexCtrl', function ($scope) {
   debugger;
 });
 
-sdcGithub.controller('ServiceWorkerCtrl', function ($scope, $route) {
-  debugger;
+sdcGithub.controller('ServiceWorkerCtrl', function ($scope, $location) {
+  $scope.fetchUrl = $location.search().fetchUrl;
+  $scope.$watch('fetchUrl', function() {
+    $location.search({'fetchUrl': $scope.fetchUrl});
+  });
+  $scope.fetchInfo = $location.search().fetchInfo;
+  $scope.$watch('fetchInfo', function() {
+    $location.search({'fetchInfo': $scope.fetchInfo});
+    var fetchInfo = $filter('json')($scope.fetchInfo);
+    if ($scope.fetchInfo != fetchInfo) {
+      $scope.fetchInfo = fetchInfo;
+    }
+  });
 });
 
 sdcGithub.config(['$routeProvider', function($routeProvider) {
