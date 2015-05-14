8290fc1ad5be13ae3feba48f63276082702b0b51
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-05-14 04:43:33 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 self.addEventListener('fetch', function(event) {
   var url = new URL(event.request.url);
-  if (url.pathname.match(/\/fetch$/))
+  if (url.pathname.match(/\/sw\/fetch$/))
     event.respondWith(fetch(new Request(
       unescape(url.search.match(/(?:&|^\?)url=([^&]+)/)[1]),
       JSON.parse(unescape(url.search.match(/(?:&|^\?)init=([^&]+)/)[1])))));
-  if (url.pathname.match(/\/response$/))
+  if (url.pathname.match(/\/sw\/response$/))
     event.respondWith(new Response(
       unescape(url.search.match(/(?:&|^\?)response=([^&]+)/)[1]),
       JSON.parse(unescape(url.search.match(/(?:&|^\?)init=([^&]+)/)[1]))));
