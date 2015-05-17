676c2ac886e5b3fe410f48c7067f5d5779a5d5c8
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-05-17 05:01:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,32 @@ sdcGithub.controller('ServiceWorkerCtrl', function ($scope, $location, $filter) 
       $scope.serviceWorkerUrl,
       JSON.parse($scope.serviceWorkerInit));
   };
+  // XMLHttpRequest
+  $scope.xhrHeader = {};
+  $scope.logXhrProgress = function(prefix, target) {
+    function log(type, message) {
+      $scope.apply(function(scope) {
+        var logEntry = {type: type, message: message};
+        scope.xhrLog.push(logEntry);
+        console.log(logEntry);
+      });
+    }
+    var events = ['loadstart', 'progress', 'abort', 'error', 'load', 'timeout', 'loadend', 'readystatechange'];
+    for (var i=0; i<events.length; i++) {
+      target.addEventListener(events[i], log.bind(null, prefix + events[i]));
+    }
+  };
+  $scope.fetchXhr = function() {
+    var xhr = new XMLHttpRequest;
+    xhr.open($scope.xhrMethod, $scope.xhrUrl, $scope.xhrAsync, $scope.xhrUsername, $scope.xhrPassword);
+    for (header in $scope.xhrHeader) {
+      xhr.setRequestHeader(header, $scope.xhrHeader[header]);
+    }
+    xhr.withCredentials = $scope.xhrWithCredentials;
+    $scope.logXhrProgress('XHR.upload.', xhr.upload);
+    $scope.logXhrProgress('XHR.', xhr);
+    xhr.send($scope.xhrData);
+  };
 });
 
 sdcGithub.config(['$routeProvider', function($routeProvider) {
