ed620125a42cc2c69ccc404171746788bf6c835a
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-05-18 23:39:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,24 +8,6 @@ sdcGithub.controller('IndexCtrl', function ($scope) {
   debugger;
 });
 
-sdcGithub.controller('FetchCtrl', function ($scope, $location, $filter) {
-  $scope.fetchUrl = $location.search().fetchUrl;
-  $scope.fetchInfo = $location.search().fetchInfo;
-  $scope.doFetch = function() {
-    $scope.fetchInfo = $filter('json')(JSON.parse($scope.fetchInfo));
-    $location.search({fetchUrl: $scope.fetchUrl, fetchInfo: $scope.fetchInfo});
-    fetch($scope.fetchUrl, JSON.parse($scope.fetchInfo)).then(function(response) {
-      $scope.$apply(function(scope) {
-        scope.fetchResponse = response;
-      });
-    }).catch(function(error) {
-      $scope.$apply(function(scope) {
-        scope.fetchError = error;
-      });
-    });
-  };
-});
-
 sdcGithub.controller('ServiceWorkerCtrl', function ($scope, $location, $filter) {
   $scope.serviceWorkerController = navigator.serviceWorker.controller;
   $scope.serviceWorkerUrl = '/sw.js';
@@ -91,10 +73,6 @@ sdcGithub.config(['$routeProvider', '$sceDelegateProvider', function($routeProvi
     when('/', {
       templateUrl: 'partials/index.html',
       controller: 'IndexCtrl'
-    }).
-    when('/fetch', {
-      templateUrl: 'partials/fetch.html',
-      controller: 'FetchCtrl'
     }).
     when('/service-worker', {
       templateUrl: 'partials/service-worker.html',
