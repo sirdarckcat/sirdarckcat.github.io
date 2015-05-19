7b632465ce135df4eab9bde83e300b892cfd7aa3
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-05-19 14:55:38 +0200

    Update sw.cached.js

--- manifest/sw.cached.js
+++ manifest/sw.cached.js
@@ -1,1 +1,9 @@
-debugger;
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open('default').then(function(cache) {
+    return cache.addAll([new Request('index.html', {mode: 'no-cors'}]));
+  }));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request));
+});
