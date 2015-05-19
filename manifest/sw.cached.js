141f328a092948f67488a72640715c556488f511
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-05-19 15:35:14 +0200

    Update sw.cached.js

--- manifest/sw.cached.js
+++ manifest/sw.cached.js
@@ -5,5 +5,8 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response) return response;
+    return fetch(event.request);
+  }));
 });
