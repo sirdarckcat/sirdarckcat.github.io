c166e6df06bbc689d7183deb17393e724b965f8b
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-06-27 10:39:06 -0700

    Update sw.js

--- range/sw.js
+++ range/sw.js
@@ -1,6 +1,37 @@
 onfetch = function(e) {
   var url = new URL(e.request.url);
   console.log('got a request for', url.href);
+  // Range Lab
+  var steps, stepsMatch;
+  if (stepsMatch = url.search.match(/[?&]steps=([^&]+)/)) {
+    steps = stepsMatch[1];
+    console.log('request has steps=', steps);
+    var ranges = {};
+    steps.split(/;/).forEach(function(step) {
+      var parts = step.split(':');
+      ranges[parts[0]]=parts;
+    });
+    var step, range = e.request.headers.get('range');
+    if (step = ranges[range]) {
+      switch(step[1]) {
+        case 'response':
+          e.respondWith(new Response(atob(step[2]), JSON.parse(unescape(step[3]))));
+          break;
+        case 'fetch':
+          e.respondWith(fetch(unescape(step[2]), JSON.parse(unescape(step[3]))));
+          break;
+        case 'fetch+response':
+          e.respondWith(
+            fetch(unescape(step[2]), JSON.parse(unescape(step[3]))).then(function(res) {
+              return res.blob();
+            }).then(function(b) {
+              return new Response(b.slice(step[4], step[5]), JSON.parse(unescape(step[6])));
+            }));
+      }
+    }
+    return;
+  }
+  // Basic truncation example
   if (url.pathname == '/wikipedia/ru/c/c6/Glorious.ogg') {
     if (e.request.headers.get('range') == 'bytes=0-') {
       console.log('responding with fake response');
@@ -10,5 +41,6 @@ onfetch = function(e) {
     } else {
       console.log('ignoring request');
     }
+    return;
   }
 };
