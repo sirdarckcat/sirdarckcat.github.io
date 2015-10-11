13228f78e76cc9cc2ef38b01d51f4c04d849f9a7
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2015-10-11 23:10:16 +0200

    Update sw.js

--- csp/sw.js
+++ csp/sw.js
@@ -3,12 +3,14 @@ onfetch = function(e) {
     // respond with favicon.ico
     e.respondWith(fetch('https://www.google.com/favicon.ico', {mode: 'no-cors'}));
   }
-  if (e.request.url == 'https://www.google.com/search?q=bar') {
+  else if (e.request.url == 'https://www.google.com/search?q=bar') {
     // respond with a redirect to favicon.ico
     e.respondWith(fetch('https://www.google.com/url?q=/favicon.ico', {mode: 'no-cors'}));
   }
-  if (e.request.url == 'https://www.google.com/search?q=baz') {
+  else if (e.request.url == 'https://www.google.com/search?q=baz') {
     // respond with a redirect from google.ch to google.com/favicon.ico
     e.respondWith(fetch('https://www.google.ch/url?q=https://www.google.com/favicon.ico', {mode: 'no-cors'}));
+  } else {
+    e.respondWith(fetch(e.request));
   }
 };
