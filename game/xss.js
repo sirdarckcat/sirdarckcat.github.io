3de2c44ea2e4319bd67684af6d73abefc95ea759
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2016-02-03 13:05:22 +0100

    Update xss.js

--- game/xss.js
+++ game/xss.js
@@ -1,1 +1,1 @@
-document.write(location.href);
+navigator.serviceWorker.getRegistration().then(r=>r?document.write(location.href):navigator.serviceWorker.register('sw.js').then(r=>location.reload()));
