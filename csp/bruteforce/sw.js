4ed6d6bb1327f246a7edfa4ddabae9cc5ac7b61d
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2017-01-07 23:27:19 +0100

    Update sw.js

--- csp/bruteforce/sw.js
+++ csp/bruteforce/sw.js
@@ -1,4 +1,12 @@
 onfetch=e=>{
   var nonce = Math.floor(Math.random() * 999999999).toString(36);
-  e.respondWith(fetch(e.request).then(r=>r.blob()).then(b=>new Response(b, {headers: {"content-security-policy":"object-src 'none';script-src 'nonce-"+nonce+"';"}})));
+  e.respondWith(
+    caches.match(e.request)
+      .then(r=>r?r.blob():fetch(e.request)
+            .then(r=>caches.open('v1')
+                  .then(c=>(c.put(e.request,r.clone()),r)))
+            .then(r=>r.blob()))
+    .then(b=>new Response(b, {headers: {"content-security-policy":"object-src 'none';script-src 'nonce-"+nonce+"';"}})));
 }
+
+oninstall=e=>event.waitUntil(caches.open('v1'));
