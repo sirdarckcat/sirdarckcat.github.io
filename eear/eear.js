81884736258ae4727a8a04e00e8639abdd58b738
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2022-01-29 15:35:32 +0100

    select the correct audio

--- eear/eear.js
+++ eear/eear.js
@@ -9,20 +9,13 @@ onload = async function () {
   const audioDevices = devices.filter(device=>device.kind=='audioinput');
   console.log(audioDevices);
   media.getTracks().forEach(track=>track.stop());
-  const deviceStreams = await Promise.all(audioDevices.map(audioDevice=>navigator.mediaDevices.getUserMedia({audio: {exact: audioDevice.deviceId}})));
+  const deviceStreams = await Promise.all(audioDevices.map(audioDevice=>navigator.mediaDevices.getUserMedia({audio: {deviceId: {exact: audioDevice.deviceId}}})));
   console.log(deviceStreams);
   const audioContext = new AudioContext();
-  await audioContext.resume();
   console.log(audioContext);
   await audioContext.audioWorklet.addModule("processor1.js");
   const workletNode = new AudioWorkletNode(audioContext, "my-audio-processor", {numberOfInputs: deviceStreams.length});
   console.log(workletNode);
   deviceStreams.forEach((deviceStream, index)=>audioContext.createMediaStreamSource(deviceStream).connect(workletNode, 0, index));
-  // deviceStreams.forEach((deviceStream, index)=>{
-  //   const oscillatorNode = audioContext.createOscillator();
-  //   oscillatorNode.type = 'square';
-  //   oscillatorNode.frequency.setValueAtTime(3000, audioContext.currentTime + index);
-  //   oscillatorNode.connect(workletNode, 0, index);
-  //   oscillatorNode.start();
-  // });
+  await audioContext.resume();
 };
