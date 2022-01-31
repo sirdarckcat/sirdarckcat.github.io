a4e3c59f9d0b06055afdb30d403faf649c245a56
Eduardo' Vela" Nava (sirdarckcat) <evn@google.com>
2022-01-31 15:15:10 +0100

    Update worker.js

--- eear/worker.js
+++ eear/worker.js
@@ -63,7 +63,7 @@ class TDOAWorker {
     const max_tau = distance / sound_speed;
 
     const tau = this.gccPhat(mic1, mic2, this.sampleRate, max_tau);
-    return { tau, m: (max_tau * distance) / tau.tau, theta: Math.asin(tau.tau / max_tau) * 180 / Math.PI };
+    return { tau, theta: Math.asin(tau.tau / max_tau) * 180 / Math.PI };
   }
 };
 
