db8947e45ab2385aca476e60103b0f95bd76c93b
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2023-05-04 11:02:38 +0200

    remove exact constraint for latency

--- binaural-recording/binaural-recording.js
+++ binaural-recording/binaural-recording.js
@@ -41,7 +41,7 @@ onload = async function () {
           autoGainControl: false,
           channelCount: 1,
           echoCancellation: {exact: false},
-          latency: {exact: 0},
+          latency: 0,
           noiseSuppression: false,
           sampleRate: 48000,
           sampleSize: 16,
